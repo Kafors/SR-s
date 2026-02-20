06a5c0f4fdc3b7bfd9fcca15812c27937c8edbbe
Oleksiy <ole.rudenko@istu.edu.ua>
2026-02-20 09:33:37 +0200

    дороблена 3 задача

--- Third SR/ex3.cpp
+++ Third SR/ex3.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 struct Distance
 {
-    int cells;      
-    double pixels;  
+    int cells;
+    double pixels;
 };
 
 Distance maxDistance(const Distance& d1, const Distance& d2)
@@ -21,11 +22,17 @@ int main()
 {
     Distance d1, d2;
 
-    cout << "Введіть першу відстань (клітинки пікселі): ";
+    cout << "Введіть першу відстань (cells pixels): ";
     cin >> d1.cells >> d1.pixels;
 
-    cout << "Введіть другу відстань (клітинки пікселі): ";
+    cout << "Введіть другу відстань (cells pixels): ";
     cin >> d2.cells >> d2.pixels;
+
+    d1.cells = abs(d1.cells);
+    d1.pixels = fabs(d1.pixels);
+
+    d2.cells = abs(d2.cells);
+    d2.pixels = fabs(d2.pixels);
 
     Distance maxD = maxDistance(d1, d2);
 
@@ -35,3 +42,4 @@ int main()
 
     return 0;
 }
+
